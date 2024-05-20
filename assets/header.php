932d57b5b4083a15fb4955fc087e1465a34e4e50
sannija <117991126+saannija@users.noreply.github.com>
2024-05-20 16:14:08 +0300

    added jaunums page

--- assets/header.php
+++ assets/header.php
@@ -1,8 +1,8 @@
     <header>
         <a href="#" class="logo">IT Spēks</a>
         <nav id="navbar">
-            <a href="#">Sākumlapa</a>
-            <a href="#services">Pakalpojumi</a>
+            <a href="index.php">Sākumlapa</a>
+            <a href="index.php#services">Pakalpojumi</a>
             <a href="vakances.php">Vakances</a>
             <a href="jaunumi.php">Jaunumi</a>
             <a href="#">Kontakti</a>
---
+++ jaunums.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IT Spēks</title>
+    <link rel="stylesheet" href="assets/style_main.css">
+    <!-- <link rel="shortcut icon" href="images/lvt.png" type="image/x-icon"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
+</head>
+<body>
+    <?php
+        require "assets/header.php";
+    ?>
+
+    <section id="headerNew">
+        <h1>Virsraksts</h1>
+    </section>
+
+    <section id="new">
+        <img src="images/example2.jpg" alt="img">
+        <p class="allText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum corrupti quas quis temporibus itaque accusamus doloremque excepturi expedita deserunt corporis. Debitis, suscipit. Ab animi veritatis at, dolorum repudiandae ipsa ducimus quo delectus nemo in beatae nesciunt quasi doloribus alias odio aperiam, quod quam maxime. Commodi fugit doloremque quam, placeat rerum asperiores molestiae inventore culpa harum vero, ut expedita aperiam.
+            <br><br>
+        Minus est laborum suscipit explicabo corrupti repudiandae cupiditate commodi. Iste ut in autem inventore eligendi beatae quibusdam explicabo sint pariatur, molestias a officia minus voluptatibus optio. Aliquam consequatur nostrum officiis facere cumque vero, illum suscipit, aut esse soluta, deserunt tempora consectetur incidunt fugiat velit necessitatibus. Voluptatum, vero, maxime delectus inventore cumque commodi, consequuntur fugiat in esse perferendis beatae! Sapiente ad itaque obcaecati, eligendi quae provident voluptatum ut.
+            <br><br>
+        Sint repellat expedita deserunt iste maiores nisi a nemo quis odio! Labore nisi dignissimos, provident tenetur nobis earum corrupti numquam quidem libero quas consequatur officiis quos architecto excepturi, ea pariatur ipsum similique magnam consectetur aliquam quasi adipisci cumque harum. Animi, dolor nesciunt nemo natus labore amet excepturi. Suscipit molestias ut dicta iure totam voluptate voluptas corporis voluptatem qui aspernatur deleniti, eaque officiis recusandae cupiditate ipsum sapiente. Id maiores est amet quibusdam similique. Suscipit, voluptate quaerat voluptas beatae maxime sequi excepturi totam at quas dolorum earum nesciunt tenetur minus! Perferendis provident, ullam quisquam itaque, cumque aperiam labore dicta sed error obcaecati animi. Sit eaque asperiores explicabo. Numquam commodi quaerat dolorem veniam reiciendis possimus porro magnam iusto eligendi ratione? Sit voluptatum a eius saepe eligendi. Voluptas repellat laboriosam repudiandae, voluptatem eligendi similique cumque soluta exercitationem. Consequuntur, non ipsum est, corporis laudantium ullam praesentium delectus totam officia magnam ipsam at sequi. Harum omnis voluptas laudantium quo corrupti ea eaque atque fugiat voluptatibus. 
+            <br><br>
+        Nisi esse unde reiciendis velit. Recusandae sint quam veniam aliquam a! Similique adipisci a tempora iure blanditiis rerum, eaque numquam aspernatur enim? Recusandae et laboriosam ducimus repudiandae magni corporis eaque. Ipsam, quidem quam maxime soluta sint odio tempore nihil doloribus eaque iste quisquam veritatis, dolores harum. Error iste, incidunt accusamus veritatis nulla, est esse ipsam sit rem dolores dicta laudantium praesentium repellendus in. Impedit rem veritatis provident veniam assumenda nostrum consectetur quo, possimus, iure asperiores voluptates animi aliquam dolorum dolor et, nihil vel repellat corporis pariatur tenetur suscipit nemo? Perspiciatis, temporibus, veritatis perferendis fugit corrupti modi reprehenderit voluptate nesciunt tempore suscipit cupiditate alias aperiam. Repellat sequi minima id sunt ab facilis nisi iure alias temporibus laboriosam a, quis omnis numquam aperiam ipsa rem. Adipisci cumque fuga quis? Eos officiis minus accusantium laboriosam similique laudantium esse magni sequi neque in delectus nam nihil molestias tempore odit consectetur aliquid excepturi, impedit alias? 
+            <br><br>
+        Delectus doloremque dolorem consequuntur impedit dicta dolore consectetur velit necessitatibus ea nam in totam neque ullam eum iure est omnis nostrum, autem nihil eaque quaerat ipsam, dolor harum architecto. Veritatis, quod rerum. Libero corporis voluptatum doloribus, explicabo eos suscipit. Praesentium corporis vitae itaque accusamus sequi. Blanditiis repellendus explicabo veritatis eius ea quasi! Architecto quisquam dignissimos obcaecati ex molestias, praesentium asperiores unde repellat totam recusandae ut.</p>
+    </section>
+    <hr>
+    <section id="footerNew">
+        <p id="author">Vārds Uzvārds</p>
+        <p id="date">Publicēts: 20.05.2024.</p>
+    </section>
+   <?php
+        require "assets/footer.php";
+    ?>
+</body>
+</html>
