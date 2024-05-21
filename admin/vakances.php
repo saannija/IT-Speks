7cf999d1211f0abed281c250572bd3643a81fac7
Vlada <vlada.rmv@gmail.com>
2024-05-21 12:03:08 +0300

    Modified table in vakances.php

--- admin/vakances.php
+++ admin/vakances.php
@@ -5,7 +5,8 @@
 
     <section id="admin-section">
         <div class="table-heading">Pieejamās vakances</div>
-        <table>
+        <table id="vacancies-table">
+        <tr>
             <th>Logo</th>
             <th>Profesijas nosaukums</th>
             <th>Kompānijas nosaukums</th>
@@ -16,13 +17,38 @@
             <th>Apraksts</th>
             <th>Datums</th>
             <th></th>
+        </tr>
 
-            <!-- <td>
-                <form method='post' action='vakance.php'>
-                    <button type='submit' name='edit' class='btn' value='{$piet['Audzeknis_ID']}'><i class='fas fa-edit'></i></button>
-                    <button type='submit' name='detele' class='btn' value='{$piet['Audzeknis_ID']}'><i class='fas fa-times'></i></button>
-                </form>
-            </td> -->
+        <?php
+            for($i = 0; $i < 9; $i++){
+                echo "
+                    <tr>
+                        <td id='image'>
+                            <div class='logo-container'>
+                                <img src='../images/example.jpg' class='default-borders'>
+                            </div>
+                        </td>
+                        <td>Profesijas nosaukums</td>
+                        <td>Kompānijas nosaukums</td>
+                        <td>Darba vieta</td>
+                        <td>Atrašanās vieta</td>
+                        <td>Slodze</td>
+                        <td>Alga</td>
+                        <td>Apraksts</td>
+                        <td>Datums</td>
+                        <td>
+                            <form method='post' action='vakance.php'>
+                                <button type='submit' name='edit' class='default-button'><i class='fas fa-edit'></i></button>
+                                <button type='submit' name='detele' class='default-button'><i class='fas fa-times'></i></button>
+                            </form>
+                        </td>
+                    </tr>
+                ";
+            }
+        
+        ?>
+        <!-- value='{$piet['Audzeknis_ID']}' -->
+
         </table>
     </section>
     
