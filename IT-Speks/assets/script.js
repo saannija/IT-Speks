297b5726d97cec385215e1d4163f33eb74d53139
Romazanova <vlada.rmv@gmail.com>
2024-05-27 09:45:13 +0300

    Added connect_db

--- IT-Speks/assets/script.js
+++
@@ -1,8 +0,0 @@
-if ( window.history.replaceState ) {
-    window.history.replaceState( null, null, window.location.href );
-}
-
-function togglePanel() {
-    let panel = document.getElementById("searchbar-container");
-    panel.classList.toggle("expanded");
-}
---
+++ assets/connect_db.php
@@ -0,0 +1,37 @@
+<?php
+    ini_set('display_errors', 1);
+    $serveris = "localhost";
+    $lietotajs = "grobina1_romazanova";
+    $parole = "8Xzai1FC!";
+    $datubaze = "grobina1_romazanova";
+
+    $savienojums = mysqli_connect($serveris, $lietotajs, $parole, $datubaze);
+
+    if(!$savienojums){
+        echo "Savienojums ar DB nav izveidots!";
+    }else{
+        echo "Savienojums ar DB veiksmigs!";
+    }
+
+    // if(isset($_POST["autorizacija"])){
+    //     session_start();
+    //     $username = mysqli_real_escape_string($savienojums, $_POST["lietotajs"]);
+    //     $password = mysqli_real_escape_string($savienojums, $_POST["parole"]);
+    //     $sql_teikums = "SELECT * FROM uznemsana_lietotaji WHERE Lietotajvards = '$username'";
+    //     $result = mysqli_query($savienojums, $sql_teikums);
+
+    //     if(mysqli_num_rows($result) == 1){
+    //         while($user = mysqli_fetch_array($result)){
+    //             if(password_verify($password, $user['Parole'])){
+    //                 $_SESSION["lietotajvards"] = $user['Lietotajvards'];
+    //                 header("location:./admin/index.php");
+    //                 exit;
+    //             }else{
+    //                 echo "Nepareizs lietotājvārds vai parole!";
+    //             }
+    //         }
+    //     }else{
+    //         echo "Nepareizs lietotājvārds vai parole!";
+    //     }
+    // }
+?>
